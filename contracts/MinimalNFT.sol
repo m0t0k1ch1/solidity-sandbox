3f0912bb7f1cdce5cdd6bda8d2f391b668290521
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2023-12-19 22:44:19 +0900

    feat: implement minimal nft contract

---
+++ contracts/MinimalNFT.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract MinimalNFT is ERC721URIStorage {
+    uint256 private _nextTokenID;
+
+    constructor() ERC721("MinimalNFT", "MNFT") {}
+
+    function mint(string memory tokenURI_) external {
+        _mint(msg.sender, _nextTokenID);
+        _setTokenURI(_nextTokenID, tokenURI_);
+        _nextTokenID++;
+    }
+}
