e7a163114824ca90302949bf7537630a5165b9fc
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2023-12-21 17:39:58 +0900

    refactor(contracts/nft-locker): rename function: call -> execute

--- contracts/nft-locker/Account.sol
+++ contracts/nft-locker/Account.sol
@@ -1,4 +1,83 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.23;
 
-contract Account {}
+import {Context} from "@openzeppelin/contracts/utils/Context.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IERC721Receiver} from "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+
+error UnauthorizedModule(address module);
+
+contract Account is Context, Ownable, IERC721Receiver {
+    event ModuleAuthorized(address indexed module);
+    event ModuleUnauthorized(address indexed module);
+
+    event Executed(
+        address indexed module,
+        address indexed to,
+        uint256 value,
+        bytes data
+    );
+
+    mapping(address module => bool) private _isModuleAuthorizeds;
+
+    constructor(
+        address initialOwner_,
+        address[] memory modules_
+    ) Ownable(initialOwner_) {
+        for (uint256 i = 0; i < modules_.length; i++) {
+            _isModuleAuthorizeds[modules_[i]] = true;
+        }
+    }
+
+    modifier onlyAuthorizedModule() {
+        if (!_isModuleAuthorizeds[_msgSender()]) {
+            revert UnauthorizedModule(_msgSender());
+        }
+        _;
+    }
+
+    function isModuleAuthorized(address module_) public view returns (bool) {
+        return _isModuleAuthorizeds[module_];
+    }
+
+    function authorizeModule(address module_) external onlyAuthorizedModule {
+        _isModuleAuthorizeds[module_] = true;
+
+        emit ModuleAuthorized(module_);
+    }
+
+    function unauthorizeModule(address module_) external onlyAuthorizedModule {
+        _isModuleAuthorizeds[module_] = false;
+
+        emit ModuleUnauthorized(module_);
+    }
+
+    function execute(
+        address to_,
+        uint256 value_,
+        bytes calldata data_
+    ) external payable onlyAuthorizedModule returns (bytes memory) {
+        (bool success, bytes memory result) = to_.call{value: value_}(data_);
+        if (!success) {
+            assembly {
+                returndatacopy(0, 0, returndatasize())
+                revert(0, returndatasize())
+            }
+        }
+
+        emit Executed(_msgSender(), to_, value_, data_);
+
+        return result;
+    }
+
+    function onERC721Received(
+        address,
+        address,
+        uint256,
+        bytes memory
+    ) external pure override returns (bytes4) {
+        return this.onERC721Received.selector;
+    }
+
+    receive() external payable {}
+}
---
+++ test/nft-locker/Account.test.ts
@@ -0,0 +1,128 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
+
+import {
+  Account,
+  Account__factory,
+  NFT,
+  NFT__factory,
+} from "../../typechain-types";
+
+describe("Account", () => {
+  let runner: HardhatEthersSigner;
+  let owner: HardhatEthersSigner;
+  let dummyModule1: HardhatEthersSigner;
+  let dummyModule2: HardhatEthersSigner;
+  let minter: HardhatEthersSigner;
+
+  let account: Account;
+
+  before(async () => {
+    [runner, owner, dummyModule1, dummyModule2, minter] =
+      await ethers.getSigners();
+  });
+
+  beforeEach(async () => {
+    const accountFactory = (await ethers.getContractFactory(
+      "contracts/nft-locker/Account.sol:Account"
+    )) as Account__factory;
+    account = await accountFactory.deploy(owner.address, [
+      dummyModule1.address,
+    ]);
+    await account.waitForDeployment();
+  });
+
+  it("initial state", async () => {
+    expect(await account.owner()).to.equal(owner.address);
+    expect(await account.isModuleAuthorized(dummyModule1.address)).to.be.true;
+    expect(await account.isModuleAuthorized(dummyModule2.address)).to.be.false;
+  });
+
+  describe("authorizeModule, unauthorizedModule", () => {
+    it("failure: UnauthorizedModule", async () => {
+      await expect(
+        account.connect(dummyModule2).authorizeModule(dummyModule2.address)
+      )
+        .to.be.revertedWithCustomError(account, "UnauthorizedModule")
+        .withArgs(dummyModule2.address);
+
+      await expect(
+        account.connect(dummyModule2).unauthorizeModule(dummyModule1.address)
+      )
+        .to.be.revertedWithCustomError(account, "UnauthorizedModule")
+        .withArgs(dummyModule2.address);
+    });
+
+    it("success", async () => {
+      // authorize dummy module 2 by dummy module 1
+      {
+        await expect(
+          account.connect(dummyModule1).authorizeModule(dummyModule2.address)
+        )
+          .to.emit(account, "ModuleAuthorized")
+          .withArgs(dummyModule2.address);
+
+        expect(await account.isModuleAuthorized(dummyModule2.address)).to.be
+          .true;
+      }
+
+      // unauthorize dummy module 1 by dummy module 2
+      {
+        await expect(
+          account.connect(dummyModule2).unauthorizeModule(dummyModule1.address)
+        )
+          .to.emit(account, "ModuleUnauthorized")
+          .withArgs(dummyModule1.address);
+
+        expect(await account.isModuleAuthorized(dummyModule1.address)).to.be
+          .false;
+      }
+    });
+  });
+
+  describe("execute", () => {
+    let nft: NFT;
+
+    beforeEach(async () => {
+      const nftFactory = (await ethers.getContractFactory(
+        "contracts/nft-locker/NFT.sol:NFT"
+      )) as NFT__factory;
+      nft = await nftFactory.deploy(minter.address);
+      await nft.waitForDeployment();
+
+      await nft.connect(minter).safeAirdrop(await account.getAddress(), "");
+    });
+
+    it("failure: UnauthorizedModule", async () => {
+      await expect(
+        account
+          .connect(dummyModule2)
+          .execute(
+            await nft.getAddress(),
+            0,
+            nft.interface.encodeFunctionData("approve", [ethers.ZeroAddress, 0])
+          )
+      )
+        .to.be.revertedWithCustomError(account, "UnauthorizedModule")
+        .withArgs(dummyModule2.address);
+    });
+
+    it("success", async () => {
+      await expect(
+        account
+          .connect(dummyModule1)
+          .execute(
+            await nft.getAddress(),
+            0,
+            nft.interface.encodeFunctionData("approve", [ethers.ZeroAddress, 0])
+          )
+      )
+        .to.emit(nft, "Approval")
+        .withArgs(await account.getAddress(), ethers.ZeroAddress, 0);
+
+      expect(await nft.getApproved(0)).to.equal(ethers.ZeroAddress);
+    });
+  });
+});
