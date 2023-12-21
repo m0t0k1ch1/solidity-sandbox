84ab9e1b0166d3ac2dc7640a065f61c08c084114
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2023-12-21 15:54:32 +0900

    feat(contracts/nft-locker): use context

--- contracts/nft-locker/NFT.sol
+++ contracts/nft-locker/NFT.sol
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.23;
 
+import {Context} from "@openzeppelin/contracts/utils/Context.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract NFT is Ownable, ERC721URIStorage {
+contract NFT is Context, Ownable, ERC721URIStorage {
     uint256 private _nextTokenID;
 
     constructor(
--- contracts/nft-locker/NFTReceiver.sol
+++ contracts/nft-locker/NFTReceiver.sol
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.23;
 
+import {Context} from "@openzeppelin/contracts/utils/Context.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import {IERC721Receiver} from "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 
-contract NFTReceiver is Ownable, IERC721Receiver {
+contract NFTReceiver is Context, Ownable, IERC721Receiver {
     constructor(address initialOwner_) Ownable(initialOwner_) {}
 
     function onERC721Received(
@@ -14,7 +15,7 @@ contract NFTReceiver is Ownable, IERC721Receiver {
         uint256 tokenID_,
         bytes memory data_
     ) external override returns (bytes4) {
-        IERC721(msg.sender).safeTransferFrom(
+        IERC721(_msgSender()).safeTransferFrom(
             address(this),
             owner(),
             tokenID_,
