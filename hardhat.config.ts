532ecd1cbbeccc07110ea6ca6033c4b21ba9fef2
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2024-03-07 15:18:27 +0900

    build: bump solidity from 0.8.23 to 0.8.24

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,7 +3,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.23",
+  solidity: "0.8.24",
   networks: {
     sepolia: {
       url: "https://rpc.sepolia.org",
