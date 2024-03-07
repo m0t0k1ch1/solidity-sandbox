218f51bfe2784e3066193b0e8396a7d2aeca17e2
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2024-03-07 15:52:36 +0900

    feat(wallet): define interfaces

---
+++ contracts/wallet/IPlugin.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+interface IPlugin {
+    /// @notice Initialize plugin data for the modular account.
+    /// @dev Called by the modular account during `installPlugin`.
+    /// @param data Optional bytes array to be decoded and used by the plugin to setup initial plugin data for the modular account.
+    function onInstall(bytes calldata data) external;
+
+    /// @notice Clear plugin data for the modular account.
+    /// @dev Called by the modular account during `uninstallPlugin`.
+    /// @param data Optional bytes array to be decoded and used by the plugin to clear plugin data for the modular account.
+    function onUninstall(bytes calldata data) external;
+
+    /// @notice Run the pre execution hook specified by the `functionId`.
+    /// @dev To indicate the entire call should revert, the function MUST revert.
+    /// @param functionId An identifier that routes the call to different internal implementations, should there be more than one.
+    /// @param sender The caller address.
+    /// @param value The call value.
+    /// @param data The calldata sent.
+    /// @return Context to pass to a post execution hook, if present. An empty bytes array MAY be returned.
+    function preExecutionHook(
+        uint8 functionId,
+        address sender,
+        uint256 value,
+        bytes calldata data
+    ) external returns (bytes memory);
+
+    /// @notice Run the post execution hook specified by the `functionId`.
+    /// @dev To indicate the entire call should revert, the function MUST revert.
+    /// @param functionId An identifier that routes the call to different internal implementations, should there be more than one.
+    /// @param preExecHookData The context returned by its associated pre execution hook.
+    function postExecutionHook(
+        uint8 functionId,
+        bytes calldata preExecHookData
+    ) external;
+}
